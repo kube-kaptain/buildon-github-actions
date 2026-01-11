45acf712e8506c4ecf7866bdc181d947a5c772ae
Fred Cooke <fred.cooke@gmail.com>
2026-01-12 00:40:53 +1300

    Last of the vars out of hoook scripts and a few others - this is probably the best we can do.

--- src/scripts/defaults/hooks-leftovers.bash
+++ src/scripts/defaults/hooks-leftovers.bash
@@ -7,9 +7,19 @@
 #
 # shellcheck disable=SC2034  # Variables used by sourcing scripts
 
-# Docker target config (defaults come from workflow/action layer)
+# Docker config (defaults come from workflow/action layer)
 DOCKER_TARGET_REGISTRY="${DOCKER_TARGET_REGISTRY:-}"
 DOCKER_TARGET_BASE_PATH="${DOCKER_TARGET_BASE_PATH:-}"
+DOCKER_REGISTRY_LOGINS="${DOCKER_REGISTRY_LOGINS:-}"
+DOCKER_PUSH_TARGETS="${DOCKER_PUSH_TARGETS:-}"
+
+# Manifests packaging
+MANIFESTS_REPO_PROVIDER_TYPE="${MANIFESTS_REPO_PROVIDER_TYPE:-}"
+MANIFESTS_PACKAGING_BASE_IMAGE="${MANIFESTS_PACKAGING_BASE_IMAGE:-}"
+
+# Release branches
+RELEASE_BRANCH="${RELEASE_BRANCH:-}"
+ADDITIONAL_RELEASE_BRANCHES="${ADDITIONAL_RELEASE_BRANCHES:-}"
 
 # Kubernetes generation (defaults in generators, hooks just pass through)
 # Kubernetes Generation Globals
