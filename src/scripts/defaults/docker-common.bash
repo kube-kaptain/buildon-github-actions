a8e0901a1ab6e5517214e6603db50bb8ab0b2017
Fred Cooke <fred.cooke@gmail.com>
2026-02-20 23:12:14 +1300

    Rename a defaults script and all the source sites that use it ready for the next change.

--- src/scripts/defaults/docker-common.bash
+++ src/scripts/defaults/docker-common.bash
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2025-2026 Kaptain contributors (Fred Cooke)
 #
-# image-build-command.bash - Default for container runtime command
+# docker-common.bash - Common defaults for all docker/podman operations
 #
 
 IMAGE_BUILD_COMMAND="${IMAGE_BUILD_COMMAND:?IMAGE_BUILD_COMMAND is required - run validate-tooling first}"
