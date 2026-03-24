b2f0363e1776c38775fe8d24aa39946aabe371bc
Fred Cooke <fred.cooke@gmail.com>
2026-03-24 22:26:13 +1300

    Upgrade the spec build to handle multiple files and have more hooks and do more things right.

---
+++ src/scripts/defaults/spec-validate.bash
@@ -0,0 +1,4 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025-2026 Kaptain contributors (Fred Cooke)
+SPEC_VALIDATION_TYPE="${SPEC_VALIDATION_TYPE:-basic}"
+SPEC_JSON_SCHEMA_URL="${SPEC_JSON_SCHEMA_URL:-https://json-schema.org/draft-07/schema}"
