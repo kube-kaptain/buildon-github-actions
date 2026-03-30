fc5309e172a8bfbaa8e4976a083e5d58e525d22e
Fred Cooke <fred.cooke@gmail.com>
2026-03-31 01:53:06 +1300

    Fix the spec build - needs to be this way due to the mixed usage context.

--- src/scripts/defaults/spec-validate.bash
+++ src/scripts/defaults/spec-validate.bash
@@ -1,3 +1,4 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2025-2026 Kaptain contributors (Fred Cooke)
-SPEC_JSON_SCHEMA_URL="${SPEC_JSON_SCHEMA_URL:-src/schemas/json-schema-draft-2020-12.json}"
+SPEC_VALIDATE_DEFAULTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SPEC_JSON_SCHEMA_URL="${SPEC_JSON_SCHEMA_URL:-${SPEC_VALIDATE_DEFAULTS_DIR}/../../schemas/json-schema-draft-2020-12.json}"
