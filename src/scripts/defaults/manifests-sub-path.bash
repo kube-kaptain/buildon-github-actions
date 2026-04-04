9ff983a821b4f39ae89e3956ca17a8079c6fd9aa
Fred Cooke <fred.cooke@gmail.com>
2026-04-05 00:46:37 +1300

    Fix the usage of the manifests build dirs - it was quite a mess - this change doesn't affect any kaptain projects - I'll verify other org projects don't have issues with this later. It's better now.

--- src/scripts/defaults/manifests-sub-path.bash
+++ src/scripts/defaults/manifests-sub-path.bash
@@ -2,8 +2,12 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2025-2026 Kaptain contributors (Fred Cooke)
 #
-# manifests-sub-path.bash - Default for source manifests directory
+# manifests-sub-path.bash - Default for source and output manifests directories
 #
 # shellcheck disable=SC2034  # Variables used by sourcing scripts
 
 MANIFESTS_SUB_PATH="${MANIFESTS_SUB_PATH:-src/kubernetes}"
+MANIFESTS_COMBINED_SUB_PATH="${OUTPUT_SUB_PATH}/manifests/combined"
+MANIFESTS_CONFIG_SUB_PATH="${OUTPUT_SUB_PATH}/manifests/config"
+MANIFESTS_SUBSTITUTED_SUB_PATH="${OUTPUT_SUB_PATH}/manifests/substituted"
+MANIFESTS_ZIP_SUB_PATH="${OUTPUT_SUB_PATH}/manifests/zip"
